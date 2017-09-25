571a1309d85a70918a2a4cf8b7fd2cd312ce394c
Guthrie <Molly.Guthrie@bissell.com>
2017-09-25 19:58:35 -0400

    commit

---
+++ Lab2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("please enter an integer between 1 and 100:");
+            int a = int.Parse(Console.ReadLine());
+
+            //if integer is odd
+
+            if (a % 2 != 0)
+            {
+                Console.WriteLine("Odd");
+            }
+            else if ((a % 2 == 0) && (a <= 25))
+            {
+                Console.WriteLine("Even and less than 25");
+            }
+
+            else if ((a % 2 == 0) && (a >= 100))
+            {
+                Console.WriteLine("Even");
+            }
+        }
+    }
+}
